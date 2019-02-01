255f4412803547dc34b75b3651cd436f539308b2
jdm79 <james.malvern@gmail.com>
2019-01-31 17:03:33 -0700

    feat: adds the Question and QuestionBank models

---
+++ Quizzler/QuestionBank.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionBank.swift
+//  Quizzler
+//
+//  Created by James Daniel Malvern on 31/01/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class QuestionBank {
+    
+    var list = [Question]()
+    
+    init()
+}
